3aea007a3d00c688f69c667e864f15ad5b4c4718
Hiroaki ITO <mail@isudzumi.com>
2021-12-04 22:32:33 +0900

    Make lockless
    2.8MiB/s

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::env;
+use std::io::{stdout, Write};
 
 fn main() {
     let expletive = env::args().nth(1).unwrap_or("y".into());
+    let out = stdout();
+    let mut out = out.lock();
     loop {
-        println!("{}", expletive);
+        writeln!(out, "{}", expletive).expect("Failed to write an expletive");
     }
 }
