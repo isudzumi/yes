846dc14131ac5c46927cdf5756979c2577a882fb
Hiroaki ITO <mail@isudzumi.com>
2021-12-04 23:02:26 +0900

    Buffering
    67MiB/s

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::env;
-use std::io::{stdout, Write};
+use std::io::{stdout, Write, BufWriter};
 
 fn main() {
     let expletive = env::args().nth(1).unwrap_or("y".into());
     let out = stdout();
-    let mut out = out.lock();
+    let mut out = BufWriter::new(out.lock());
     loop {
         writeln!(out, "{}", expletive).expect("Failed to write an expletive");
     }
