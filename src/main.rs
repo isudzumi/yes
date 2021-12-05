e8176aef712725eacdadb7b5312f3b13bfdc9701
Hiroaki ITO <mail@isudzumi.com>
2021-12-05 20:22:02 +0900

    Implement buffer
    3.3GiB/s

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
-use std::io::{stdout, Write, BufWriter};
+use std::io::{stdout, Write, BufWriter };
+
+const BUFFER_CAPACITY: usize = 64 * 1024;
 
 fn main() {
     let expletive = env::args().nth(1).unwrap_or("y".into());
     let out = stdout();
     let mut out = BufWriter::new(out.lock());
+    let expletive =  expletive + "\n";
+    let buffer = expletive.repeat(BUFFER_CAPACITY / expletive.len());
     loop {
-        writeln!(out, "{}", expletive).expect("Failed to write an expletive");
+        out.write_all(buffer.as_bytes()).expect("Failed to write an expletive")
     }
 }
